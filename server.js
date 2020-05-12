f32dc67847415aa5a8334891380ccbe7e6a566af
Melissa Galush <galushmn@gmail.com>
2020-05-11 19:54:23 -0700

    weather complete

--- server.js
+++ server.js
@@ -15,28 +15,33 @@ app.use(cors());
 app.get('/location', (req, res) => {
   console.log('location server is running');
   const locationData = require('./data/location.json');
+  const displayName = locationData[0].display_name;
   const city = req.query.city;
-  let location = new Location(locationData[0], city) 
+  const longitude = locationData[0].lon;
+  const latitude = locationData[0].lat;
+  let location = new Location(displayName, city, longitude, latitude);
   res.send(location);
 });
-
-// const coordinates = [];
-// const latitude = locationData[0].lat;
-// const longitude = locationData[0].lon;
-// coordinates.push(latitude, longitude);
 
 app.get('/weather', (req, res) => {
   console.log('weather server is running');
   const weatherData = require('./data/weather.json');
-  
-  res.send();
+  const forecast = weatherData.data[0].weather.description;
+  const time = weatherData.data[0].valid_date;
+  const location = new Weather(forecast, time);
+  res.send(location);
 });
 
-function Location(location, city) {
+function Location(displayName, city, longitude, latitude) {
   this.search_query = city;
-  this.formatted_query = location.display_name;
-  this.longitude = location.lon;
-  this.latitude = location.lat;
+  this.formatted_query = displayName;
+  this.longitude = longitude;
+  this.latitude = latitude;
+}
+
+function Weather(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
 }
 
 
