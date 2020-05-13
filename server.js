8cd020f3ffd9db9f75918070cb162b3a81d0b25c
Melissa Galush <galushmn@gmail.com>
2020-05-12 21:29:17 -0700

    trails path complete but missing date functionality

--- server.js
+++ server.js
@@ -15,12 +15,10 @@ app.use(cors());
 
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
+app.get('/trails', getTrails);
 app.get('*', sendError);
 
 function getLocation(req, res) {
-  const locationData = require('./data/location.json');
-  const displayName = locationData[0].display_name;
-
   const city = req.query.city;
   if (!city) {
     res.status(404).send('You need to pass in a city query parameter');
@@ -32,6 +30,7 @@ function getLocation(req, res) {
     key: process.env.GEOCODE_API_KEY,
     format: 'json'
   };
+
   superagent.get(url)
     .query(queryParameters)
     .then(resultFromSuper => {
@@ -48,8 +47,6 @@ function getLocation(req, res) {
 }
 
 function getWeather(req, res) {
-  const weatherData = require('./data/weather.json');
-
   const queryForSuper = {
     lat: req.query.latitude,
     lon: req.query.longitude
@@ -66,7 +63,7 @@ function getWeather(req, res) {
   superagent.get(url)
     .query(queryParameters)
     .then(resultFromSuper => {
-      const dataFromJSON = JSON.parse(resultFromSuper.text);
+      const dataFromJSON = resultFromSuper.body;
       const weatherDataArray = dataFromJSON.data.map((value) => {
         const forecast = value.weather.description;
         const date = new Date(value.last_ob_time);
@@ -83,6 +80,43 @@ function getWeather(req, res) {
 
 }
 
+function getTrails(req, res) {
+  const url = 'https://www.hikingproject.com/data/get-trails';
+
+  const queryParameters = {
+    key: process.env.TRAILS_API_KEY,
+    lat: req.query.latitude,
+    lon: req.query.longitude
+  }
+
+  superagent.get(url)
+  .query(queryParameters)
+  .then(resultFromSuper => {
+    const dataFromJSON = resultFromSuper.body;
+    const trailsDataArray = dataFromJSON.trails.map((value) => {
+      const name = value.name;
+      const location = value.location;
+      const length = value.length;
+      const stars = value.stars;
+      const star_votes = value.star_votes;
+      const summary = value.summary
+      const trail_url = value.url;
+      const conditions = value.conditionDetails;
+      const date = new Date(value.conditionDate);
+      // TODO: figure out how to format date
+      const condition_date = date;
+      const condition_time = date;
+      const trail = new Trail (name, location, length, stars, star_votes, summary, trail_url, conditions, condition_date, condition_time)
+      return trail;
+    })
+    res.send(trailsDataArray);
+  })
+  .catch(error => {
+    console.log(error);
+    res.send(error).status(500);
+  });
+}
+
 function sendError(req, res) {
   res.status(500).send('This page does not exist');
 }
@@ -97,6 +131,19 @@ function Location(displayName, city, longitude, latitude) {
 function Weather(forecast, time) {
   this.forecast = forecast;
   this.time = time;
+}
+
+function Trail(name, location, length, stars, star_votes, summary, trail_url, conditions, condition_date, condition_time) {
+  this.name = name;
+  this.location = location;
+  this.length = length;
+  this.stars = stars;
+  this.star_votes = star_votes;
+  this.summary = summary;
+  this.trail_url = trail_url;
+  this.conditions = conditions;
+  this.condition_date = condition_date;
+  this.condition_time = condition_time;
 }
 
 
