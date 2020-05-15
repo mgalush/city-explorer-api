9d2926190fac839374e3b6ac7897142f54a915fc
Melissa Galush <galushmn@gmail.com>
2020-05-14 19:08:54 -0700

    removed schema.sql from .gitignore

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE locations;
+
+CREATE TABLE locations(
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  longitude NUMERIC,
+  latitude NUMERIC
+);
